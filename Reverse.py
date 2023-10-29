8e4ab33d9b3af8f61e7cf6eb33f428cd49f88eee
harshadapr <harshadapraundal97@gmail.com>
2023-10-30 01:34:27 +0530

    reverse string

---
+++ Reverse.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    if len(s) == 0:
+        return s
+    else:
+        return reverse_string(s[1:]) + s[0]
+
+original_string = "Hello, World!"
+reversed_string = reverse_string(original_string)
+print(reversed_string)  # Output will be "!dlroW ,olleH"
