bd3baa913edeb6417a3a4d89f6a16a45ce50d758
harshadapr <harshadapraundal97@gmail.com>
2023-10-31 23:30:19 +0530

    file

--- files.py
+++ files.py
@@ -8,3 +8,17 @@ for filename in os.listdir(source_directory):
     source = os.path.join(source_directory, filename)
     destination = os.path.join(destination_directory, filename)
     shutil.move(source, destination)
+
+
+import csv
+
+data = [
+    ['Name', 'Age'],
+    ['Alice', 25],
+    ['Bob', 30]
+]
+
+# Write data to a CSV file
+with open('data.csv', 'w', newline='') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(data)
