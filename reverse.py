218cae9192b27cb6519ddbf8d2bb4e081a6890a5
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-06 00:46:53 +0530

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    return s[::-1]
+
+original_string = "Hello, World!"
+reversed_string = reverse_string(original_string)
+print(f"Original: {original_string}")
+print(f"Reversed: {reversed_string}")
