778d9370fee83924abe74b58180cfd13b44c8290
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-07 10:31:08 +0530

    Update Reverse.py

--- Reverse.py
+++ Reverse.py
@@ -6,4 +6,4 @@ def reverse_string(s):
 
 original_string = "Hello, World!"
 reversed_string = reverse_string(original_string)
-print(reversed_string)  # Output will be "!dlroW ,olleH"
+print(reversed_string) 
