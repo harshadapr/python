08b5b84c0f3279db2a5459119aedc2778d92d6b2
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-07 19:39:16 +0530

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,11 @@
+to_do_list = []
+
+while True:
+    task = input("Enter a task (or 'quit' to exit): ")
+    if task == "quit":
+        break
+    to_do_list.append(task)
+
+print("To-Do List:")
+for i, task in enumerate(to_do_list, start=1):
+    print(f"{i}. {task}")
