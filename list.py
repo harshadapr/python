779ddd23ea170b5568318c11e72a5d9346cbae59
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-09 23:00:22 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5]
+
+# Calculate the sum of the list
+total = sum(numbers)
+
+# Find the maximum and minimum values in the list
+maximum = max(numbers)
+minimum = min(numbers)
+
+# Sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+print(f"List: {numbers}")
+print(f"Sum: {total}")
+print(f"Maximum: {maximum}")
+print(f"Minimum: {minimum}")
+print(f"Sorted List: {sorted_numbers}")
