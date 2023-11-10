cd7f94466485418a4cf86775e489b5651df8dcc1
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-11 00:02:10 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,8 @@
+def power(base, exponent):
+    result = base ** exponent
+    return result
+
+base_value = 2
+exponent_value = 3
+result = power(base_value, exponent_value)
+print(f"{base_value} raised to the power of {exponent_value} is {result}")
