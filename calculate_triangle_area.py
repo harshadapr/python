f1cbf87d6e93dc418ae64d5881ad800675f14712
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-20 23:45:08 +0530

    Create calculate_triangle_area.py

---
+++ calculate_triangle_area.py
@@ -0,0 +1,7 @@
+def calculate_triangle_area(base, height):
+    return 0.5 * base * height
+
+triangle_base = 8
+triangle_height = 5
+area = calculate_triangle_area(triangle_base, triangle_height)
+print(f"The area of the triangle is: {area}")
